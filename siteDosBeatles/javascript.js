9d1d4fec0619bbcdf4cc700640cdd7a623354d06
Edson Araujo <edsonaraujodesouzaneto@gmail.com>
2023-08-08 14:01:23 -0300

    update

---
+++ siteDosBeatles/javascript.js
@@ -0,0 +1,9 @@
+/*var ed = window.document.getElementById('meuInstagram')
+ed.addEventListener('mouseenter',clicou)
+ed.addEventListener('mouseout',saiu)
+function clicou() {
+    ed.innerHTML = 'Oi'
+}
+function saiu() {
+    ed.innerHTML = 'Edson Araujo'
+}*/
