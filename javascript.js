125dbc69027340746a86107a47a137b44885b7c8
edsonaraujoneto <edsonaraujodesouzaneto@gmail.com>
2024-02-16 15:04:20 -0300

    style: adicionar estilização para impressão portrait e landscape

--- javascript.js
+++
@@ -1,9 +0,0 @@
-/*var ed = window.document.getElementById('meuInstagram')
-ed.addEventListener('mouseenter',clicou)
-ed.addEventListener('mouseout',saiu)
-function clicou() {
-    ed.innerHTML = 'Oi'
-}
-function saiu() {
-    ed.innerHTML = 'Edson Araujo'
-}*/
